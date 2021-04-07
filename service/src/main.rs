b149356181e0a00bed4d9a9714fd3157751d803e
Graham Cox <graham@grahamcox.co.uk>
2021-04-07 20:35:03 +0100

    Set up Tracing

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,6 +1,24 @@
 #![deny(clippy::all, clippy::pedantic)]
 #![allow(clippy::module_name_repetitions)]
 
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
+
+#[actix_rt::main]
+async fn main() {
+    dotenv().ok();
+
+    env_logger::init();
+
+    opentelemetry::global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name(env!("CARGO_PKG_NAME"))
+        .install_simple()
+        .unwrap();
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = Registry::default().with(telemetry);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    tracing::info!("Hello, world!");
 }
