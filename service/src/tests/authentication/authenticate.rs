58dd54559f3b41840b873a35d5e325b9d217ea1c
Graham Cox <graham@grahamcox.co.uk>
2021-04-26 07:27:08 +0100

    Test that we can use the token we get from authenticating

--- service/src/tests/authentication/authenticate.rs
+++ service/src/tests/authentication/authenticate.rs
@@ -152,6 +152,7 @@ async fn wrong_password() {
 #[actix_rt::test]
 async fn correct_password() {
     let user = SeedUser {
+        user_id: "4ea96dc3-df11-43c0-8a33-a0813f03937f".parse().unwrap(),
         username: "testuser".to_owned(),
         ..SeedUser::default()
     }
@@ -178,13 +179,69 @@ async fn correct_password() {
 
     assert_json_snapshot!(response.to_json().unwrap(), {
       ".token" => "[token]",
-      ".user_id" => "[user_id]",
       ".expires_at" => "[expires_at]",
     }, @r###"
     {
       "token": "[token]",
-      "user_id": "[user_id]",
+      "user_id": "4ea96dc3-df11-43c0-8a33-a0813f03937f",
       "expires_at": "[expires_at]"
+    }
+    "###);
+}
+
+#[actix_rt::test]
+async fn use_token() {
+    let user = SeedUser {
+        user_id: "4ea96dc3-df11-43c0-8a33-a0813f03937f".parse().unwrap(),
+        version: "d61dac0c-45f2-49ed-85cc-f24bbe939404".parse().unwrap(),
+        username: "testuser".to_owned(),
+        email: "testuser@example.com".to_owned(),
+        display_name: "Test User".to_owned(),
+        ..SeedUser::default()
+    }
+    .with_password("password");
+
+    let suite = TestSuite::new().await;
+    suite.seed(&user).await;
+
+    let response = suite
+        .inject(
+            TestRequest::post()
+                .uri("/authenticate/authenticate")
+                .set_json(&json!({
+                  "username": "testuser",
+                  "password": "password"
+                }))
+                .to_request(),
+        )
+        .await;
+
+    check!(response.status == 200);
+
+    let response = response.to_json().unwrap();
+    let token = response.get("token").unwrap().as_str().unwrap();
+
+    let response = suite
+        .inject(
+            TestRequest::get()
+                .uri("/users/4ea96dc3-df11-43c0-8a33-a0813f03937f")
+                .append_header(("Authorization", format!("{}", token)))
+                .to_request(),
+        )
+        .await;
+
+    check!(response.status == 200);
+
+    check!(response.headers.get("content-type").unwrap() == "application/json");
+    check!(response.headers.get("cache-control").unwrap() == "private, max-age=3600");
+    check!(response.headers.get("etag").unwrap() == "\"d61dac0c-45f2-49ed-85cc-f24bbe939404\"");
+
+    assert_json_snapshot!(response.to_json().unwrap(), @r###"
+    {
+      "userId": "4ea96dc3-df11-43c0-8a33-a0813f03937f",
+      "username": "testuser",
+      "email": "testuser@example.com",
+      "displayName": "Test User"
     }
     "###);
 }
